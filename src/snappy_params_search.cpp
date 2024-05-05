de3e3f66f6ec1fbd63521511c81d258cbf2b5706
hli <hlibb@connect.ust.hk>
2024-05-05 19:25:20 +0800

    Add uncompress

--- src/snappy_params_search.cpp
+++ src/snappy_params_search.cpp
@@ -1,16 +1,92 @@
 #include <string>
 #include <assert.h>
+#include <filesystem>
+#include <iostream>
 
 #include "snappy.h"
+
 
 inline char* string_as_array(std::string* str) 
 {
     return str->empty() ? NULL : &*str->begin();
 }
 
-
-int main(int argc, char **argv)
+static inline int read_file(const std::string &file_name, std::string *output)
 {
+    std::FILE *fp = std::fopen(file_name.c_str(), "rb");
+    if (fp = nullptr)
+    {
+        std::perror(file_name.c_str());
+        std::exit(1);
+    }
+
+    output->clear();
+    while (!std::feof(fp))
+    {
+        char buffer[4096];
+        size_t bytes_read = std::fread(buffer, 1, sizeof(buffer), fp);
+        if (bytes_read == 0 && std::ferror(fp))
+        {
+            std::perror("fread");
+            std::exit(1);
+        }
+        output->append(buffer, bytes_read);
+    }
+
+    std::fclose(fp);
+    return 0;
+}
+
+static inline int write_file(const std::string &file_name, const std::string &content)
+{
+    std::FILE *fp = std::fopen(file_name.c_str(), "wb");
+    if (fp == nullptr)
+    {
+        std::perror(file_name.c_str());
+        std::exit(1);
+    }
+
+    size_t bytes_written = std::fwrite(content.data(), 1, content.size(), fp);
+    if (bytes_written != content.size())
+    {
+        std::perror("fwrite");
+        std::exit(1);
+    }
+
+    std::fclose(fp);
+    return 0;
+}
+
+
+int main(int argc, char **argv) try
+{
+    if (argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <searchfile>\n"
+                     "Examples:\n"
+                     "  " << argv[0] << " file.snappy" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string fullinput;
+    const std::string filename{argv[1]};
+    read_file(filename, &fullinput);
+
+    size_t uncompLength;
+    if (!snappy::GetUncompressedLength(fullinput.data(), fullinput.size(),
+                                          &uncompLength))
+    {
+        throw std::runtime_error("snappy: Couldn't get uncompressed length");
+    }
+
+    std::string uncompressed;
+    uncompressed.resize(uncompLength);
+    if (!snappy::Uncompress(fullinput.data(), fullinput.size(), &uncompressed))
+    {
+        throw std::runtime_error("snappy: Couldn't uncompress data " + filename);
+    }
+
+    
     for (int arg = 1; arg < argc; ++arg)
     {
         std::string fullinput;
@@ -61,4 +137,8 @@ int main(int argc, char **argv)
     }
     
     return 0;
+} catch (std::exception &e)
+{
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
 }

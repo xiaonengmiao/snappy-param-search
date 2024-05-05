7cefcf9fac5715076cebddae06ed6e06f446979d
hli <hlibb@connect.ust.hk>
2024-05-05 21:46:41 +0800

    Add search level

--- src/snappy_params_search.cpp
+++ src/snappy_params_search.cpp
@@ -6,6 +6,8 @@
 #include "snappy.h"
 
 
+const int n_level = 2;
+
 inline char* string_as_array(std::string* str) 
 {
     return str->empty() ? NULL : &*str->begin();
@@ -57,6 +59,38 @@ static inline int write_file(const std::string &file_name, const std::string &co
     return 0;
 }
 
+/**
+ * @brief 
+ * 
+ * @param compressed 
+ * @param uncompressed 
+ * @return int 1 ... n_level : search succeed return level
+ *             0 : cannot find a level
+ *            -1 : error
+ */
+static inline int search_level(const std::string &compressed, const std::string &uncompressed)
+{
+    for (int i_level = 1; i_level < n_level + 1; i_level++)
+    {
+        snappy::CompressionOptions level{i_level};
+        std::string recompressed;
+        recompressed.resize(snappy::MaxCompressedLength(uncompressed.size()));
+        size_t destlen;
+        snappy::RawCompress(uncompressed.data(), uncompressed.size(),
+                            string_as_array(&recompressed),
+                            &destlen, level);
+        if (destlen > snappy::MaxCompressedLength(uncompressed.size()))
+        {
+            throw std::runtime_error("");
+        }
+        recompressed.resize(destlen);
+        if (!compressed.compare(recompressed)) break;
+        return i_level;
+    }
+
+    return 0;
+}
+
 
 int main(int argc, char **argv) try
 {
@@ -74,7 +108,7 @@ int main(int argc, char **argv) try
 
     size_t uncompLength;
     if (!snappy::GetUncompressedLength(fullinput.data(), fullinput.size(),
-                                          &uncompLength))
+                                       &uncompLength))
     {
         throw std::runtime_error("snappy: Couldn't get uncompressed length");
     }
@@ -86,54 +120,13 @@ int main(int argc, char **argv) try
         throw std::runtime_error("snappy: Couldn't uncompress data " + filename);
     }
 
-    
-    for (int arg = 1; arg < argc; ++arg)
+    int level = search_level(fullinput, uncompressed);
+    assert(level >= 0 && level <= n_level);
+    if (level > 0)
     {
-        std::string fullinput;
-        std::FILE *fp = std::fopen(argv[arg], "rb");
-        if (fp == nullptr) 
-        {
-            std::perror(argv[arg]);
-            std::exit(1);
-        }
-
-        fullinput.clear();
-        while (!std::feof(fp)) 
-        {
-            char buffer[4096];
-            size_t bytes_read = std::fread(buffer, 1, sizeof(buffer), fp);
-            if (bytes_read == 0 && std::ferror(fp)) 
-            {
-                std::perror("fread");
-                std::exit(1);
-            }
-            fullinput.append(buffer, bytes_read);
-        }
-        std::fclose(fp);
-
-        std::string compressed;
-        compressed.resize(snappy::MaxCompressedLength(fullinput.size()));
-        size_t destlen;
-        snappy::RawCompress(fullinput.data(), fullinput.size(),
-                            string_as_array(&compressed),
-                            &destlen);
-        assert(destlen <= snappy::MaxCompressedLength(fullinput.size()));
-        compressed.resize(destlen);
-
-        std::string file_name = std::string(argv[arg]).append(".comp");
-        std::FILE *fpo = std::fopen(file_name.c_str(), "wb");
-        if (fpo == nullptr) {
-            std::perror(file_name.c_str());
-            std::exit(1);
-        }
-
-        size_t bytes_written = std::fwrite(compressed.data(), 1, compressed.size(), fpo);
-        if (bytes_written != compressed.size()) {
-            std::perror("fwrite");
-            std::exit(1);
-        }
-
-        std::fclose(fpo);
+        std::cout << "Compression Level is " << level << std::endl;
+    } else {
+        std::cout << "Can not find the compression level" << std::endl;
     }
     
     return 0;
